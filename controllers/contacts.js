ec9f5d601fb198858c5cc7e759b50ec3f753c732
celinefigo-dev <cmafigu@byupathway.edu>
2026-09-19 11:46:58 +0200

    Fix MongoDB database function in contacts controller

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,84 +1,154 @@
+
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
-// Get all contacts//
+// GET - All contacts
 const getAll = async (req, res) => {
   try {
-    const result = await mongodb
+    const contacts = await mongodb
       .getDb()
       .collection('contacts')
-      .find();
+      .find()
+      .toArray();
 
-    const contacts = await result.toArray();
-
-    res.setHeader('Content-Type', 'application/json');
     res.status(200).json(contacts);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
   }
 };
 
-// Get one contact by ID//
+// GET - One contact
 const getSingle = async (req, res) => {
   try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: 'Invalid contact ID' });
+    }
+
+    const contactId = new ObjectId(req.params.id);
+
+    const contact = await mongodb
+      .getDb()
+      .collection('contacts')
+      .findOne({ _id: contactId });
+
+    if (!contact) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
+
+    res.status(200).json(contact);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// POST - Create a contact
+const createContact = async (req, res) => {
+  try {
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    // All fields are required
+    if (Object.values(contact).some(
+      value => typeof value !== 'string' || !value.trim()
+    )) {
+      return res.status(400).json({
+        message: 'All fields are required'
+      });
+    }
+
+    const response = await mongodb
+      .getDb()
+      .collection('contacts')
+      .insertOne(contact);
+
+    res.status(201).json({
+      id: response.insertedId
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// PUT - Update a contact
+const updateContact = async (req, res) => {
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({
+        message: 'Invalid contact ID'
+      });
+    }
+
+    const contactId = new ObjectId(req.params.id);
+
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    if (Object.values(contact).some(
+      value => typeof value !== 'string' || !value.trim()
+    )) {
+      return res.status(400).json({
+        message: 'All fields are required'
+      });
+    }
+
+    const result = await mongodb
+      .getDb()
+      .collection('contacts')
+      .replaceOne({ _id: contactId }, contact);
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({
+        message: 'Contact not found'
+      });
+    }
+
+    res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// DELETE - Remove a contact
+const deleteContact = async (req, res) => {
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({
+        message: 'Invalid contact ID'
+      });
+    }
+
     const contactId = new ObjectId(req.params.id);
 
     const result = await mongodb
       .getDb()
       .collection('contacts')
-      .findOne({ _id: contactId });
+      .deleteOne({ _id: contactId });
 
-    if (!result) {
-      return res.status(404).json({ message: 'Contact not found' });
+    if (result.deletedCount === 0) {
+      return res.status(404).json({
+        message: 'Contact not found'
+      });
     }
 
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(result);
+    res.status(204).send();
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
   }
-};
-
-// Week 2 Assignment //
-// POST - Create a contact
-const createContact = async (req, res) => {
-  const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
-  };
-
-  const response = await mongodb.getDatabase().collection("contacts").insertOne(contact);
-
-  res.status(201).json({ id: response.insertedId });
-};
-
-// PUT - Update a contact
-const updateContact = async (req, res) => {
-  const contactId = new ObjectId(req.params.id);
-
-  const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
-  };
-
-  await mongodb.getDatabase().collection("contacts").replaceOne({ _id: contactId }, contact);
-
-  res.status(204).send();
-};
-
-// DELETE - Remove a contact
-const deleteContact = async (req, res) => {
-  const contactId = new ObjectId(req.params.id);
-
-  await mongodb.getDatabase().collection("contacts").deleteOne({ _id: contactId });
-
-  res.status(204).send();
 };
 
 module.exports = {
